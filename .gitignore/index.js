248dc7ad1baf6892a9bbe47a9c02fa723191b040
LuapMc <40924994+LuapMc@users.noreply.github.com>
2018-07-08 17:18:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -42,9 +42,65 @@ bot.on('message', msg => {
     }
 })
 
+bot.on('message', message => {
 
-case "serverlist":
-message.channel.send(bot.guilds.map(r => r.name + ` | **${r.memberCount}** membres'))
+if(message.content === prefix + "infodiscord") {
+
+var embed = new Discord.RichEmbed()
+
+.setDescription("Information du Discord")
+
+.addField("Nom Du Discord", message.guild.name)
+
+.addField("Crée le", message.guild.createdAt)
+
+.addField("Tu as rejoin le", message.member.joinedAt)
+
+.addField("Utilisateurs sur le discord", message.guild.memberCount)
+
+.setColor("0x0000FF")
+
+message.channel.sendEmbed(embed)
+
+}
+
+if (message.content.startsWith(prefix + "sondage")) {
+
+if(message.author.id == "296611013401247744"){
+
+let args = message.content.split(" ").slice(1);
+
+let thingToEcho = args.join(" ")
+
+var embed = new Discord.RichEmbed()
+
+.setDescription("Sondage")
+
+.addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+
+.setColor("0xB40404")
+
+.setTimestamp()
+
+message.guild.channels.find("name", "sondage").sendEmbed(embed)
+
+.then(function (message) { 
+
+message.react("✅")
+
+messace.react("❌")
+
+}).catch(function() {
+
+});
+
+}else{
+
+return message.reply("Tu n'as pas la permission.")
+
+}}})
+
+
 
 
 
