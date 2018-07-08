7f1f45c11241155a59558311684fceddbb82c811
LuapMc <40924994+LuapMc@users.noreply.github.com>
2018-07-08 15:46:29 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -46,43 +46,9 @@ bot.on('message', msg => {
 
 
 
-if(msg.content.startsWith(prefix + 'ban')){
 
-if(msg.channel.type === 'dm') return;
 
-if(!msg.guild.member(msg.author).hasPermission('ADMINISTRATOR')){
 
-return msg.reply("**❌ Vous n'avez pas la permissions d'utiliser cette commande**").catch(console.error);
-
-}
-
-if(msg.mentions.users.size === 0){
-
-return msg.reply("**❌ Veuillez mentionner l'utilisateur que vous voulez ban**")
-
-}
-
-if(!msg.guild.member(client.user).hasPermission('ADMINISTRATOR')){
-
-return msg.reply("**❌ Je n'ai pas la permission `ADMINISTRATOR` pour ban cet utilisateur**").catch(console.error);
-
-}
-
-let banMember = msg.guild.member(msg.mentions.users.first());
-
-if(!banMember){
-
-return msg.channel.send("**❌ Cet utilisateur n'est certainement pas valide**")
-
-}
-
-banMember.ban().then(member => {
-
-msg.channel.send(`**${member.user.username}** a bien été ban**`);
-
-})
-
-}
 
 bot.on("ready", function () {
 bot.user.setActivity("Owner : LuapMc /")
