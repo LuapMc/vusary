f9a65003486508f1a1086403dc5368609e7db03c
LuapMc <40924994+LuapMc@users.noreply.github.com>
2018-07-08 14:23:13 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,6 +8,16 @@ bot.on("message" , function (message) {
      }
 })
 
+bot.on('message', message => {
+if (message.content === prefix + "help"){
+message.channel.sendMessage("Liste des commandes: \n -/help");
+}
+
+if(message.content === "Salut"){
+message.reply("Bien le bonjour. :D");
+console.log("Commande Salut effectué");
+}
+});
 
 bot.on("ready", function () {
     bot.user.setActivity("by !LuapMc#9493")
