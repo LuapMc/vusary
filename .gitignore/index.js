163a9e99b941e9dd9874652eb17985ecad23ef16
LuapMc <40924994+LuapMc@users.noreply.github.com>
2018-07-18 12:34:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -90,10 +90,7 @@ bot.on("message" , function (message) {
 
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
 
 	 if (command === 'kick') {
 		if (!message.mentions.users.size) {
