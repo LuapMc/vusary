84c8fc2180f6b5b09f2623afc1b6e5ba3b73b472
LuapMc <40924994+LuapMc@users.noreply.github.com>
2018-07-18 12:17:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -117,42 +117,7 @@ client.on('message', message => {
 
 		message.channel.send(`First argument: ${args[0]}`);
 	}
-	else if (command === 'kick') {
-		if (!message.mentions.users.size) {
-			return message.reply('you need to tag a user in order to kick them!');
-		}
 
-		const taggedUser = message.mentions.users.first();
-
-		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-	}
-	else if (command === 'avatar') {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
-		}
-
-		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: ${user.displayAvatarURL}`;
-		});
-
-		message.channel.send(avatarList);
-	}
-	else if (command === 'prune') {
-		const amount = parseInt(args[0]) + 1;
-
-		if (isNaN(amount)) {
-			return message.reply('that doesn\'t seem to be a valid number.');
-		}
-		else if (amount <= 1 || amount > 100) {
-			return message.reply('you need to input a number between 1 and 99.');
-		}
-
-		message.channel.bulkDelete(amount, true).catch(err => {
-			console.error(err);
-			message.channel.send('there was an error trying to prune messages in this channel!');
-		});
-	}
-});
 
 
 
