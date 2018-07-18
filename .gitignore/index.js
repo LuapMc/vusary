92c4d043a8bf281b2d4e478a8d58576f2d16e7f8
LuapMc <40924994+LuapMc@users.noreply.github.com>
2018-07-18 12:23:34 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -95,35 +95,21 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		message.channel.send('Pong.');
-	}
-	else if (command === 'beep') {
-		message.channel.send('Boop.');
-	}
-	else if (command === 'server') {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	}
-	else if (command === 'user-info') {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	}
-	else if (command === 'info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
-		else if (args[0] === 'foo') {
-			return message.channel.send('bar');
+	 if (command === 'kick') {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to kick them!');
 		}
 
-		message.channel.send(`First argument: ${args[0]}`);
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
 	}
+	 if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
+		}
 
-
-
-
-
-
-
+	
 
   
 
