fdcbda98ab300c50316cd307c49d73301054411e
LuapMc <40924994+LuapMc@users.noreply.github.com>
2018-07-19 14:12:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -88,11 +88,7 @@ bot.on("message" , function (message) {
      }
 })
 
-module.exports = (client, message, args) => {
-  message.channel.send('Pong...').then((msg) => {
-    msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  });
-};
+	  
 
 	
 
